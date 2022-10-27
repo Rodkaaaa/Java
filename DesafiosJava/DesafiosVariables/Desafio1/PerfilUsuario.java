57d196a55db4433e8f30fb933c8546b8fae5b647
Rodkaaaa <frederickcid@gmail.com>
2022-10-27 09:53:08 -0300

    Ãcomentario para ayuda

--- DesafiosJava/DesafiosVariables/Desafio1/PerfilUsuario.java
+++ DesafiosJava/DesafiosVariables/Desafio1/PerfilUsuario.java
@@ -31,9 +31,14 @@ public class PerfilUsuario {
 
         System.out.println("Ingrese su Tipo de Sangre");
         String TipoSangre = consola.readLine();
-
+        //invocamos Localdate , sirve para invocar datos de fecha con algun formato(String) absorbe la fecha actual del pc
+        //en este caso absorbo mi fecha actual el cual me devuelve 27102022 , sin separar los datos
         LocalDate anioActual = LocalDate.now();
+        //damos formato del datos que necesitamos en este caso yo solo invoco el año actual de mi pc
+        //formato mas comun seria "DD-MM-YYYY" el cual transforma el dato de arriba 27102022 a 27-10-2022
+        //pero yo solo solicite el año para restarlo a la edad y sea un codigo escalable
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("YYYY");
+        // aplicamos el formato al año que absorbimos
         String anioFormatter = anioActual.format(formato);
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
@@ -41,6 +46,7 @@ public class PerfilUsuario {
         System.out.println("Preferencia laboral: " + PrefLab);
         System.out.println("Nacionalidad: " + nacionalidad);
         System.out.println("Tipo de sangre: " + TipoSangre);
+        //aplicamos variable de la fecha con el formato y le restamos la edad para saber en que año nacio
         System.out.println("año de nacimiento: "+ ( Integer.parseInt(anioFormatter) - edad ));
 
         
